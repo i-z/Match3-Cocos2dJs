a68b436526f52089497b530341cfe2e96e12900a
Dmitry Chizhov <chizhov.dee@gmail.com>
2015-10-23 00:20:57 +0500

    bla bla changes

--- src/app.js
+++ src/app.js
@@ -13,6 +13,8 @@ var touchListener;
 
 var tolerance = 400;
 
+var arrowsLayer;
+
 var game = cc.Layer.extend({
   init: function(){
     this._super();
@@ -26,6 +28,9 @@ var game = cc.Layer.extend({
     globezLayer = new cc.Layer();
 
     this.addChild(globezLayer);
+
+    arrowsLayer = new cc.DrawNode();
+    this.addChild(arrowsLayer);
 
     this.createLevel1();
 
@@ -83,13 +88,61 @@ touchListener = cc.EventListener.create({
     visitedTiles.push({row: pickedRow, col: pickedCol});
   },
   onMouseUp: function(event){
+    arrowsLayer.clear();
+
     startColor = null;
 
     for(var i = 0; i < visitedTiles.length; i++){
       var tile = visitedTiles[i];
 
-      tileArray[tile.row][tile.col].setOpacity(255);
-      tileArray[tile.row][tile.col].picked = false;
+      if(visitedTiles.length < 3) {
+        tileArray[tile.row][tile.col].setOpacity(255);
+        tileArray[tile.row][tile.col].picked = false;
+      } else {
+        globezLayer.removeChild(tileArray[tile.row][tile.col]);
+        tileArray[tile.row][tile.col] = null;
+      }
+
+    }
+
+    if(visitedTiles.length >= 3){
+      for(var i = 1; i < fieldSize; i++){
+        for(var j = 0; j < fieldSize; j++){
+          if(tileArray[i][j] != null){
+            var holesBelow = 0;
+
+            for(var k = i - 1; k >= 0; k --){
+              if(tileArray[k][j] == null){
+                holesBelow++;
+              }
+            }
+
+            if(holesBelow > 0){
+              var moveAction = cc.moveTo(0.5, new cc.Point(tileArray[i][j].x,tileArray[i][j].y - holesBelow * tileSize));
+              tileArray[i][j].runAction(moveAction);
+              tileArray[i - holesBelow][j] = tileArray[i][j];
+              tileArray[i][j] = null;
+            }
+          }
+        }
+      }
+
+      var i, j; // i - column, j - row
+      for(i = 0; i < fieldSize; i++){
+        for(j = fieldSize-1; j >= 0; j--){
+          if(tileArray[j][i] != null){
+            break;
+          }
+        }
+
+        var missingGlobes = fieldSize - 1- j;
+
+        if(missingGlobes > 0){
+          for(j = 0; j < missingGlobes; j++){
+            this.fallTile(fieldSize - j - 1, i, missingGlobes - j);
+          }
+        }
+      }
     }
 
     visitedTiles = [];
@@ -123,7 +176,44 @@ touchListener = cc.EventListener.create({
               });
             }
           }
+        } else {
+          if(visitedTiles.length >= 2 && currentRow == visitedTiles[visitedTiles.length - 2].row && currentCol == visitedTiles[visitedTiles.length - 2].col){
+            tileArray[visitedTiles[visitedTiles.length - 1].row][visitedTiles[visitedTiles.length - 1].col].setOpacity(255);
+            tileArray[visitedTiles[visitedTiles.length - 1].row][visitedTiles[visitedTiles.length - 1].col].picked = false;
+
+            visitedTiles.pop();
+          }
         }
+
+        this.drawPath();
+      }
+    }
+  },
+
+  fallTile: function(row, col, height){
+    var randomTile = Math.floor(Math.random()*tileTypes.length);
+    var spriteFrame = cc.spriteFrameCache.getSpriteFrame(tileTypes[randomTile]);
+
+    var sprite = new cc.Sprite(spriteFrame);
+    sprite.val = randomTile;
+    sprite.picked = false;
+    globezLayer.addChild(sprite, 0);
+
+    sprite.setPosition(col*tileSize+tileSize/2,(fieldSize+height)*tileSize);
+    var moveAction = cc.moveTo(0.5, new cc.Point(col*tileSize+tileSize/2,row*tileSize+tileSize/2));
+    sprite.runAction(moveAction);
+    tileArray[row][col] = sprite;
+  },
+
+  drawPath: function(){
+    arrowsLayer.clear();
+    if(visitedTiles.length>0){
+      for(var i=1;i<visitedTiles.length;i++){
+        arrowsLayer.drawSegment(
+          new cc.Point(visitedTiles[i-1].col*tileSize+tileSize/2,visitedTiles[i-1].row*tileSize+tileSize/2),
+          new cc.Point(visitedTiles[i].col*tileSize+tileSize/2,visitedTiles[i].row*tileSize+tileSize/2), 4,
+          cc.color(255, 255, 255, 255)
+        );
       }
     }
   }
